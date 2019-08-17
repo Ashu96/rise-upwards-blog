3de83fc75c82787f705e4fb864df7c001475e786
Ashutosh Bhardwaj <bhardwajashu96@hotmail.com>
2019-08-17 20:06:57 +0530

    Implement `Reports`  sections

--- src/components/Report.js
+++ src/components/Report.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Styled from 'styled-components'
+import Img from 'gatsby-image'
 import { Heading3, Heading4, Label } from '../styles/text'
 
 const ReportContainer = Styled.div`
@@ -58,11 +59,18 @@ function Report({ title, body, before_image, after_image, label }) {
       <div className="report__image-container">
         <div>
           <Heading4>Before</Heading4>
-          <img alt="chart" src={before_image.publicURL} />
+          {before_image.publicURL && <img alt="chart" src={before_image.publicURL} />}
+          {/* TODO: Replace svg image assets */}
+          {before_image.childImageSharp && (
+            <Img alt="chart" fixed={before_image.childImageSharp.fixed} />
+          )}
         </div>
         <div>
           <Heading4>After</Heading4>
-          <img alt="chart" src={after_image.publicURL} />
+          {after_image.publicURL && <img alt="chart" src={after_image.publicURL} />}
+          {after_image.childImageSharp && (
+            <Img alt="chart" fixed={after_image.childImageSharp.fixed} />
+          )}
         </div>
       </div>
       <Label className="report__label">{label}</Label>
