5bc1c893aae562ade9d34b2fd56e4b33911054fa
Ashutosh Bhardwaj <bhardwajashu96@hotmail.com>
2019-08-22 08:56:31 +0530

    Update `Blog` template

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -1,54 +1,97 @@
 import React from 'react'
 import { graphql } from 'gatsby'
+import Styled from 'styled-components'
 import Img from 'gatsby-image'
 import Layout from '../components/layout'
-import { Heading2, BodyText } from '../styles/text'
-import { Container, Row, Col } from '../styles/grid'
+import RelatedPosts from '../components/Blog/RelatedPosts'
+import { Heading1, BodyText } from '../styles/text'
+import { SectionWrapper, Row, Col } from '../styles/grid'
+import { backgrounds } from '../constants/colors'
 
-function Outcomes({ data }) {
+const BlogContentContainer = Styled.div`
+  background-color: ${backgrounds.white};
+  border-radius: 10px;
+  box-shadow: 0 4px 8px 0 rgba(219, 221, 227, 0.8);
+`
+
+function Blog({ data }) {
   const { strapiBlog } = data
+
+  const category =
+    strapiBlog &&
+    strapiBlog.blogcategories &&
+    strapiBlog.blogcategories[0] &&
+    strapiBlog.blogcategories[0].title
+
+  const relatedBlogsID =
+    strapiBlog &&
+    strapiBlog.blogcategories &&
+    strapiBlog.blogcategories[0] &&
+    strapiBlog.blogcategories[0].blogs
 
   return (
     <Layout
+      headerProps={{ bgPrimary: false }}
       showSubscribe={strapiBlog.show_subscribe_form}
       showContact={strapiBlog.show_contact_form}
     >
-      <Container>
-        <Row className="justify-content-center">
-          <Col className="col-lg-6">
-            <Heading2 textCenter className="mgn-t-60">
-              {strapiBlog.title}
-            </Heading2>
-            <Img
-              fixed={strapiBlog.image.childImageSharp.fixed}
-              className="mgn-t-40 mgn-b-40"
-              alt="nesting"
-            />
-            <BodyText className="mgn-b-60">{strapiBlog.content}</BodyText>
-          </Col>
-        </Row>
-      </Container>
+      <SectionWrapper
+        bgPrimary
+        containerProps={{
+          style: { padding: '100px 0px' }
+        }}
+      >
+        <BlogContentContainer>
+          <Row className="justify-content-center">
+            <Col className="col-lg-9">
+              <Heading1 textCenter className="mgn-t-60">
+                {strapiBlog.title}
+              </Heading1>
+              <BodyText textCenter className="mgn-t-20">
+                {strapiBlog.date} | {category}
+              </BodyText>
+
+              <Img
+                fluid={strapiBlog.image.childImageSharp.fluid}
+                className="mgn-t-100 mgn-b-40"
+                alt="nesting"
+              />
+              <BodyText className="mgn-b-60">{strapiBlog.content}</BodyText>
+            </Col>
+          </Row>
+        </BlogContentContainer>
+        <RelatedPosts relatedBlogsID={relatedBlogsID} />
+      </SectionWrapper>
     </Layout>
   )
 }
 
-export default Outcomes
+export default Blog
 
 export const query = graphql`
   query($id: String!) {
     strapiBlog(id: { eq: $id }) {
-      title
       id
+      strapiId
+      title
+      body
+      date(formatString: "MMMM DD, YYYY")
       content
+      slug
       image {
         childImageSharp {
-          fixed(width: 480) {
-            ...GatsbyImageSharpFixed
+          fluid(maxWidth: 770) {
+            ...GatsbyImageSharpFluid
           }
         }
       }
       show_subscribe_form
       show_contact_form
+      blogcategories {
+        id
+        title
+        blogs
+      }
     }
   }
 `
