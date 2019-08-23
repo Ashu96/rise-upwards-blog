dc12271d29c8012326f800a267f742040f32cac9
Ashutosh Bhardwaj <bhardwajashu96@hotmail.com>
2019-08-23 14:33:57 +0530

    Add config and addons for storybook

---
+++ .storybook/addons.js
@@ -0,0 +1,5 @@
+import '@storybook/addon-knobs/register'
+// If you want it as a top level tab
+// import '@storybook/addon-notes/register'
+import '@storybook/addon-notes/register-panel'
+import '@storybook/addon-actions/register'
