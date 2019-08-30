2a2ad50225ce186a79b31a0402b2fd52a10d6553
Ashutosh Bhardwaj <bhardwajashu96@hotmail.com>
2019-08-30 16:42:18 +0530

    Add file generator

---
+++ generator-uprise/generators/app/index.js
@@ -0,0 +1,60 @@
+const snakeCase = require('lodash.snakecase')
+const Generator = require('yeoman-generator')
+const COMPONENT_TEMPLATE = 'component.js.template'
+const STORY_TEMPLATE = 'story.js.template'
+// const TEST_TEMPLATE = 'test.jsx'
+
+const COMPONENT_DIR = './src/components/'
+const STORY_DIR = './stories/'
+// const TEST_DIR = 'frontend/luxury-guest/tests/components/PdpFramework/sections'
+
+module.exports = class extends Generator {
+	method1() {
+		this.log('method 1 just ran')
+	}
+	_writeFile(templatePath, destinationPath, params) {
+		if (!this.fs.exists(destinationPath)) {
+			this.fs.copyTpl(templatePath, destinationPath, params)
+		}
+	}
+
+	prompting() {
+		return this.prompt([
+			{
+				type: 'input',
+				name: 'componentName',
+				required: true,
+				message:
+					'Yo! What is the section component name? (e.g. SuperFlyFullBleed or ThreeImagesWithFries)'
+			}
+		]).then(data => {
+			this.data = data
+		})
+	}
+
+	writing() {
+		const { componentName } = this.data
+		const componentConst = snakeCase(componentName).toUpperCase()
+
+		this._writeFile(
+			this.templatePath(COMPONENT_TEMPLATE),
+			this.destinationPath(COMPONENT_DIR, `${componentName}.js`),
+			{ componentConst, componentName }
+		)
+
+		this._writeFile(
+			this.templatePath(STORY_TEMPLATE),
+			this.destinationPath(STORY_DIR, `${componentName}.stories.jsx`),
+			{ componentName, componentPath: `${COMPONENT_DIR}${componentName}` }
+		)
+
+		// this._writeFile(
+		// 	this.templatePath(TEST_TEMPLATE),
+		// 	this.destinationPath(TEST_DIR, `${componentName}.test.jsx`),
+		// 	{ componentName }
+		// )
+
+		// this._addToSectionTypes()
+		// this._addToSectionMapping()
+	}
+}
