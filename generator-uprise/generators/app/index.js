92997c60f8e232c54c8030d242c3ddb32bac49e0
Ashutosh Bhardwaj <bhardwajashu96@hotmail.com>
2019-09-01 17:20:22 +0530

    Add `container` template

--- generator-uprise/generators/app/index.js
+++ generator-uprise/generators/app/index.js
@@ -2,6 +2,7 @@ const snakeCase = require('lodash.snakecase')
 const Generator = require('yeoman-generator')
 const COMPONENT_TEMPLATE = 'component.js.template'
 const STORY_TEMPLATE = 'story.js.template'
+const CONTAINER_TEMPLATE = 'container.js.template'
 // const TEST_TEMPLATE = 'test.jsx'
 
 const COMPONENT_DIR = './src/components/'
@@ -38,14 +39,26 @@ module.exports = class extends Generator {
 
 		this._writeFile(
 			this.templatePath(COMPONENT_TEMPLATE),
-			this.destinationPath(COMPONENT_DIR, `${componentName}.js`),
+			this.destinationPath(
+				`${COMPONENT_DIR}${componentName}`,
+				`${componentName}.js`
+			),
+			{ componentConst, componentName }
+		)
+
+		this._writeFile(
+			this.templatePath(CONTAINER_TEMPLATE),
+			this.destinationPath(`${COMPONENT_DIR}${componentName}`, 'index.js'),
 			{ componentConst, componentName }
 		)
 
 		this._writeFile(
 			this.templatePath(STORY_TEMPLATE),
 			this.destinationPath(STORY_DIR, `${componentName}.stories.jsx`),
-			{ componentName, componentPath: `${COMPONENT_DIR}${componentName}` }
+			{
+				componentName,
+				componentPath: `${COMPONENT_DIR}${componentName}/${componentName}`
+			}
 		)
 
 		// this._writeFile(
